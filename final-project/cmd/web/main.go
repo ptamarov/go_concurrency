2c7a8536f15301aff33366e628fdbe5ee7f08189
ptamarov <ptamarov@gmail.com>
2023-05-24 15:54:56 +0200

    Updating gitignore.

---
+++ final-project/cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to a database
+
+	// create sessions
+
+	// create channels
+
+	// create a waitgroup
+
+	// set the application config
+
+	// set up mail
+
+	// listen to web connection
+
+}
